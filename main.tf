23711c67965005b368813903dae594835411a3c8
vietnh2202 <vietnh2202@gmail.com>
2025-05-19 10:32:53 +0700

    Update Terraform backend region to us-east-1

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ terraform {
   backend "s3" {
     bucket       = "viet-terraform"
     key          = "vpc-lab/terraform.tfstate"
-    region       = var.region
+    region       = "us-east-1"
     use_lockfile = true
   }
 }
