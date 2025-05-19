f886e76627b691850a086397274b12b28f370026
vietnh2202 <vietnh2202@gmail.com>
2025-05-19 11:26:59 +0700

    Remove AWS access key and secret key variables from Terraform configuration

--- variables.tf
+++ variables.tf
@@ -1,15 +1,3 @@
-variable "aws_access_key" {
-  description = "AWS Access Key"
-  type        = string
-  sensitive   = true
-}
-
-variable "aws_secret_key" {
-  description = "AWS Secret Key"
-  type        = string
-  sensitive   = true
-}
-
 variable "region" {
   description = "AWS Region"
   type        = string
