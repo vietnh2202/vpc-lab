344fc5fa544f26bf8185b0a786da04b642f8bda4
vietnh2202 <vietnh2202@gmail.com>
2025-05-19 10:12:03 +0700

    Add AWS credentials verification and update variable sensitivity

--- main.tf
+++ main.tf
@@ -15,9 +15,7 @@ terraform {
 }
 
 provider "aws" {
-  region     = "us-east-1"
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
+  region = "us-east-1"
 }
 
 # Create a VPC with public and private subnets
