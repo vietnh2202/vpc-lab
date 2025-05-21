0b152f18aa63117ef4fd3d61a389ddb048ec2d05
vietnh2202 <vietnh2202@gmail.com>
2025-05-21 15:08:55 +0700

    Refactor AWS setup

--- main.tf
+++ main.tf
@@ -26,19 +26,22 @@ module "vpc" {
   name = "my-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-east-1a"]
-  public_subnets  = ["10.0.0.0/20"]
+  azs             = ["${var.region}a", "${var.region}b"]
+  public_subnets  = ["10.0.0.0/20", "10.0.16.0/20"]
   private_subnets = ["10.0.128.0/20"]
 
-  enable_nat_gateway = false
-  single_nat_gateway = false
+  enable_nat_gateway     = true
+  single_nat_gateway     = true
+  one_nat_gateway_per_az = false
+  enable_dns_support     = true
+  enable_dns_hostnames   = true
 
   tags = {
     Name = "my-vpc"
   }
 }
 
-# Security group
+# Security groups
 resource "aws_security_group" "alb_sg" {
   name        = "my-alb-sg"
   description = "Allow HTTP traffic"
@@ -54,6 +57,13 @@ resource "aws_security_group" "alb_sg" {
     }
   }
 
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   tags = {
     Name = "my-alb-sg"
   }
@@ -61,7 +71,7 @@ resource "aws_security_group" "alb_sg" {
 
 resource "aws_security_group" "ecs_sg" {
   name        = "my-ecs-sg"
-  description = "Allow traffic from ALB"
+  description = "Security group for ECS instances"
   vpc_id      = module.vpc.vpc_id
 
   ingress {
@@ -71,8 +81,161 @@ resource "aws_security_group" "ecs_sg" {
     security_groups = [aws_security_group.alb_sg.id]
   }
 
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   tags = {
     Name = "my-ecs-sg"
+  }
+}
+
+# IAM Roles
+resource "aws_iam_role" "ecs_instance_role" {
+  name = "ecs_instance_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_instance_role_policy" {
+  role       = aws_iam_role.ecs_instance_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
+}
+
+resource "aws_iam_instance_profile" "ecs_instance_profile" {
+  name = "ecs_instance_profile"
+  role = aws_iam_role.ecs_instance_role.name
+}
+
+resource "aws_iam_role" "ecs_task_role" {
+  name = "ecs_task_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_role_policy" {
+  role       = aws_iam_role.ecs_task_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+# ECS Cluster
+resource "aws_ecs_cluster" "main" {
+  name = "my-ecs-cluster"
+
+  tags = {
+    Name = "my-ecs-cluster"
+  }
+}
+
+resource "aws_launch_template" "ecs_instance" {
+  name_prefix   = "ecs-instance-"
+  image_id      = "ami-09f7b4e0bfd3103dd"
+  instance_type = "t2.micro"
+
+  network_interfaces {
+    associate_public_ip_address = false
+    security_groups             = [aws_security_group.ecs_sg.id]
+    subnet_id                   = module.vpc.private_subnets[0]
+  }
+
+  iam_instance_profile {
+    name = aws_iam_instance_profile.ecs_instance_profile.name
+  }
+
+  user_data = base64encode(<<-EOF
+              #!/bin/bash
+              echo ECS_CLUSTER=${aws_ecs_cluster.main.name} >> /etc/ecs/ecs.config
+              EOF
+  )
+
+  tag_specifications {
+    resource_type = "instance"
+    tags = {
+      Name = "ecs-instance"
+    }
+  }
+}
+
+resource "aws_autoscaling_group" "ecs_asg" {
+  desired_capacity    = 1
+  max_size            = 1
+  min_size            = 1
+  vpc_zone_identifier = [module.vpc.private_subnets[0]]
+  launch_template {
+    id      = aws_launch_template.ecs_instance.id
+    version = "$Latest"
+  }
+
+  tag {
+    key                 = "Name"
+    value               = "ecs-instance"
+    propagate_at_launch = true
+  }
+}
+
+# ECS Task Definition
+resource "aws_ecs_task_definition" "nginx" {
+  family                   = "nginx-task"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["EC2"]
+  cpu                      = "512"
+  memory                   = "512"
+  execution_role_arn       = aws_iam_role.ecs_task_role.arn
+  task_role_arn            = aws_iam_role.ecs_task_role.arn
+
+  container_definitions = jsonencode([
+    {
+      name      = "nginx"
+      image     = var.ecr_image
+      essential = true
+      portMappings = [
+        {
+          containerPort = 80
+          hostPort      = 80
+          protocol      = "tcp"
+        }
+      ]
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          "awslogs-group"         = "/ecs/nginx"
+          "awslogs-region"        = var.region
+          "awslogs-stream-prefix" = "nginx"
+        }
+      }
+    }
+  ])
+}
+
+resource "aws_cloudwatch_log_group" "nginx" {
+  name              = "/ecs/nginx"
+  retention_in_days = 1
+
+  tags = {
+    Name = "nginx-log-group"
   }
 }
 
@@ -94,7 +257,7 @@ resource "aws_lb_target_group" "ecs" {
   port        = 80
   protocol    = "HTTP"
   vpc_id      = module.vpc.vpc_id
-  target_type = "instance"
+  target_type = "ip"
 
   health_check {
     path                = "/"
@@ -122,90 +285,6 @@ resource "aws_lb_listener" "http" {
   }
 }
 
-# ECS Cluster
-resource "aws_ecs_cluster" "main" {
-  name = "my-ecs-cluster"
-
-  tags = {
-    Name = "my-ecs-cluster"
-  }
-}
-
-resource "aws_instance" "ecs_instance" {
-  ami           = "ami-0953476d60561c955"
-  instance_type = "t2.micro"
-  metadata_options {
-    http_tokens                 = "required"
-    http_put_response_hop_limit = 1
-  }
-  subnet_id              = module.vpc.public_subnets[0]
-  vpc_security_group_ids = [aws_security_group.ecs_sg.id]
-  iam_instance_profile   = aws_iam_instance_profile.ecs_instance.name
-  user_data              = <<EOF
-#!/bin/bash
-echo ECS_CLUSTER=${aws_ecs_cluster.main.name} >> /etc/ecs/ecs.config
-yum update -y
-yum install -y ecs-init
-systemctl start ecs
-systemctl enable ecs
-EOF
-
-  tags = {
-    Name = "ecs-instance"
-  }
-}
-
-# IAM Role for ECS Instance
-resource "aws_iam_role" "ecs_instance_role" {
-  name = "ecs_instance_role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "ecs_instance_role_policy" {
-  role       = aws_iam_role.ecs_instance_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
-}
-
-resource "aws_iam_instance_profile" "ecs_instance" {
-  name = "ecs_instance_profile"
-  role = aws_iam_role.ecs_instance_role.name
-}
-
-# ECS Task Definition
-resource "aws_ecs_task_definition" "nginx" {
-  family                   = "nginx-task"
-  network_mode             = "bridge"
-  requires_compatibilities = ["EC2"]
-
-  container_definitions = jsonencode([
-    {
-      name      = "nginx"
-      image     = var.ecr_image
-      cpu       = 100
-      memory    = 128
-      essential = true
-      portMappings = [
-        {
-          containerPort = 80
-          hostPort      = 80
-          protocol      = "tcp"
-        }
-      ]
-    }
-  ])
-}
-
 # ECS Service
 resource "aws_ecs_service" "nginx" {
   name            = "nginx-service"
@@ -214,6 +293,12 @@ resource "aws_ecs_service" "nginx" {
   desired_count   = 1
   launch_type     = "EC2"
 
+  network_configuration {
+    subnets          = module.vpc.private_subnets
+    security_groups  = [aws_security_group.ecs_sg.id]
+    assign_public_ip = false
+  }
+
   load_balancer {
     target_group_arn = aws_lb_target_group.ecs.arn
     container_name   = "nginx"
@@ -221,33 +306,6 @@ resource "aws_ecs_service" "nginx" {
   }
 
   depends_on = [aws_lb_listener.http]
-}
-
-# VPC Endpoints for ECR
-resource "aws_vpc_endpoint" "ecr_api" {
-  vpc_id              = module.vpc.vpc_id
-  service_name        = "com.amazonaws.${var.region}.ecr.api"
-  vpc_endpoint_type   = "Interface"
-  subnet_ids          = module.vpc.private_subnets
-  security_group_ids  = [aws_security_group.ecs_sg.id]
-  private_dns_enabled = true
-
-  tags = {
-    Name = "ecr-api-endpoint"
-  }
-}
-
-resource "aws_vpc_endpoint" "ecr_dkr" {
-  vpc_id              = module.vpc.vpc_id
-  service_name        = "com.amazonaws.${var.region}.ecr.dkr"
-  vpc_endpoint_type   = "Interface"
-  subnet_ids          = module.vpc.private_subnets
-  security_group_ids  = [aws_security_group.ecs_sg.id]
-  private_dns_enabled = true
-
-  tags = {
-    Name = "ecr-dkr-endpoint"
-  }
 }
 
 # Outputs
